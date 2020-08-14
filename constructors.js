fee0caba340b9b0514a96b0d7979861b93a46f2f
Kyle Hansen <1hansen.kyle@gmail.com>
2020-08-13 19:12:51 -0600

    javascript 4 afternoon project

--- constructors.js
+++ constructors.js
@@ -133,11 +133,11 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
-  User.prototype.changePostRating = function (num1, num2){
-    const index = this.savedPosts.findIndex(function(e){
-      return e.id === num1
-    })
-    console.log(index[rating])
-    this.savedPosts.splice(index[rating], 1, num2)
-    
-  }
+ User.prototype.changePostRating = function(id, rating) {
+   this.savedPosts = this.savedPosts.map(element => {
+     if (element.id === id){
+     element.rating = rating
+     }
+     return element
+   })
+ }
